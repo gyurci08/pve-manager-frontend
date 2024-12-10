ca40c1557cea142507e8ed139a9d4c9146a45082
György Jandzsó <haver1036@gmail.com>
2024-12-10 23:28:12 +0100

    home, operation pages

---
+++ src/app/operation/operation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operation',
+  imports: [],
+  templateUrl: './operation.component.html',
+  styleUrl: './operation.component.scss'
+})
+export class OperationComponent {
+
+}
