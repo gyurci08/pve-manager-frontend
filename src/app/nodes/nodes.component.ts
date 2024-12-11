146353cf6a7f8a2beb0464b32a1ecd39dd241690
György Jandzsó <haver1036@gmail.com>
2024-12-11 15:58:02 +0100

    table

--- src/app/nodes/nodes.component.ts
+++ src/app/nodes/nodes.component.ts
@@ -3,24 +3,26 @@ import {NodeService} from '../../service/node.service';
 import {CommonModule} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-nodes',
-  imports: [CommonModule, MatCardModule, MatListModule],
+  imports: [CommonModule, MatCardModule, MatListModule,MatTableModule],
   templateUrl: './nodes.component.html',
   styleUrl: './nodes.component.scss'
 })
 export class NodesComponent implements OnInit{
+
   constructor(private nodeService: NodeService) {}
 
-  nodes: any[]=[];
+  displayedColumns: string[] = ['node', 'status', 'maxcpu', 'mem'];
+  nodes: any[] = [];
 
   ngOnInit() {
     this.fetchNodes();
   }
 
   fetchNodes() {
-    console.log("Fetching data")
     this.nodeService.getNodes().subscribe({
       next: (data) => {
         this.nodes = data;
